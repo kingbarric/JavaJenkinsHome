88749ae11ebb0aa795bbd4be7e59a4584feeca69
Eric Barima <kingbarric@yahoo.com>
2024-08-29 00:34:29 +0100

    first commit

---
+++ src/main/java/com/jenkins/javajenkinshome/JavaJenkinsHomeApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.javajenkinshome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJenkinsHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJenkinsHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/javajenkinshome/JavaJenkinsHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.javajenkinshome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJenkinsHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
